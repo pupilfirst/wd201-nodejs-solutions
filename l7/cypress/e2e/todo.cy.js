24ef4dde1faf286b3383bfa46451f6f6ff533b3b
Bodhish Thomas <bodhish@gmail.com>
2022-10-04 18:33:50 -0700

    Update todo.cy.js

--- l7/cypress/e2e/todo.cy.js
+++ l7/cypress/e2e/todo.cy.js
@@ -3,15 +3,19 @@ describe("Todo Express application", () => {
     cy.visit(Cypress.env("STUDENT_SUBMISSION_URL"));
   });
 
+  it("contains a header h1 tag with content - This is my Todo Application", () => {
+    cy.get('header').within(($header) => {
+      cy.contains("This is my Todo Application");
+    })
+  });
+  
   it("contains a table element to list the todo items", () => {
     cy.get("table").should("be.visible");
   });
-
-  it("contains a header h1 tag with content - This is my Todo Application", () => {
-    cy.contains("This is my Todo Application");
-  });
-
+  
   it("contains a footer h1 tag with content - Built with Node.js", () => {
-    cy.contains("Built with Node.js");
+    cy.get('footer').within(($footer) => {
+      cy.contains("Built with Node.js");
+    })
   });
 });
